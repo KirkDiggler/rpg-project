2d5c6618dc9eb9bbfa8a473432596a6e73a7e624
Kirk Gilmore <kirklgilmore@gmail.com>
2026-07-25 18:08:19 -0700

    chore: remove temporary Pi runtime copy

--- .pi/extensions/pi-team-harness/index.ts
+++
@@ -1,5 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function piTeamHarnessSkeleton(_pi: ExtensionAPI): void {
-  // PIH-1 intentionally provides only loadable module boundaries.
-}
